a722c729d5f60354d4e4be0a007493f98bdf2a4d
Chandan S <chandans@Chandans-MacBook-Air.local>
2024-04-11 15:00:20 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,10 @@
 package demo;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -33,7 +36,7 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
     }
 
@@ -49,8 +52,106 @@ public class TestCases {
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/");
+        String url= driver.getCurrentUrl();
+        if(url.contains("leetcode"))
+        {
+            System.out.println("testcase is passed as url contains leetcode");
+        }
+        else{
+            System.out.println("testcase is failed as url does not contain leetcode");
+
+        }
+
+        System.out.println("end Test case: testCase01");
+    }
+
+    public  void testCase02(){
+        System.out.println("Start Test case: testCase02");
+        // driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/");
+        WebElement ques= driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        ques.click();
+
+        String purl= driver.getCurrentUrl();
+        if(purl.contains("problemset"))
+        {
+            System.out.println("testcase is passed as url contains problemset");
+        }
+        else{
+            System.out.println("testcase is failed as url does not contain problemset");
+
+        }
+        WebElement ele;
+        String title;
+        for(int i=3; i<8;i++)
+          {
+            ele=driver.findElement(By.xpath("(//div[@role='row'])["+i+"]"));
+            title=ele.findElement(By.tagName("a")).getText();
+            System.out.println(title);
+
+          }
         System.out.println("end Test case: testCase02");
     }
+
+
+    public  void testCase03(){
+        System.out.println("Start Test case: testCase03");
+        // driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/");
+        WebElement ques= driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        ques.click();
+
+       
+        WebElement ele=driver.findElement(By.xpath("(//div[@role='row'])[3]"));
+        ele.click();
+        String url= driver.getCurrentUrl();
+        if(url.contains("two-sum"))
+        {
+            System.out.println("testcase is passed as url contains two-sum");
+        }
+        else{
+            System.out.println("testcase is failed as url does not contain two-sum");
+
+        }
+
+       
+        System.out.println("end Test case: testCase03");
+    }
+
+    public  void testCase04(){
+        System.out.println("Start Test case: testCase04");
+         driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/");
+        WebElement ques= driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        ques.click();
+
+       
+        WebElement ele=driver.findElement(By.xpath("(//div[@role='row'])[3]"));
+        ele.click();
+        WebElement ele1=driver.findElement(By.id("submissions_tab"));
+        ele1.click();
+        
+        WebElement ele2=driver.findElement(By.xpath("//a[text()='Register or Sign In']"));
+        String sin=ele2.getText();
+        System.out.println(sin);
+        if(sin.contains("Register or Sign In"))
+        {
+            System.out.println(" The test case is passed The 'Register or Sign In' button is displayed");
+
+        }
+        else{
+
+            System.out.println(" The test case is failed The 'Register or Sign In' button is not displayed");
+
+        }
+      
+
+       
+        System.out.println("end Test case: testCase04");
+    }
+
+
 
 
 }
